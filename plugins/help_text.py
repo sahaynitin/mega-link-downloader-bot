8e8c7506cacccabf4610292810c7a37880858b15
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-05 18:05:39 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -2,7 +2,7 @@ import os
 if bool(os.environ.get("WEBHOOK", False)):
     from Config import Config
 else:
-    from config import Config
+    from sample_config import Config
 from translation import Translation
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
