8344152f371e02d3b1ddc295b4df64461c3f5845
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-05 17:39:47 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -66,3 +66,20 @@ The thumbnail you send will be used for your next uploads!</b>
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS =  "Downloaded in <b>{}</b> seconds.\n\nUploaded in <b>{}</b> seconds.\n\n<b>Thanks For Using Me</b>"
     SAVED_CUSTOM_THUMB_NAIL = "Custom Thumbnail Saved. This Image Will Be Used in Your Next Upload 📁.\n\nIf you want to delete it send\n /deletethumbnail anytime!"
     DEL_ETED_CUSTOM_THUMB_NAIL = "Custom Thumbnail Cleared Successfully 🌀.\nYou will now get an auto generated thumbnail for your video uploads!"
+    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
+    SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
+URL | filename | username | password"""
+
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @xurluploaderbot)"
+    
+    
+    CUSTOM_CAPTION_UL_FILE = "{}"
+    NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
+    
+
+    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /genthumbnail to a media album, to generate custom thumbnail"
+    ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
+    CANCEL_STR = "Process Cancelled"
+    ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
+    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
